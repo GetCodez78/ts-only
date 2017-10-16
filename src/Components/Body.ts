8c1b3174190e6a8a76de35f3c0161708d7949f7c
GetCodez78 <ziheng.li@gmail.com>
2017-10-15 20:34:02 -0400

    new stuff

--- src/Components/Body.ts
+++ src/Components/Body.ts
@@ -1,10 +1,16 @@
+import Closure from './Closure';
 
 export class Body {
-    constructor(func?:()=>void) {
-      this.render();
+    constructor(func?: () => void) {
+        this.render();
     }
+
     render() {
+        
         let content = document.body.innerHTML = `<h1>This is from the Body</h1>`;
+        let closure = new Closure();
+        closure.test();
+
         return content;
     }
 
--- src/Components/Closure.ts
+++ src/Components/Closure.ts
@@ -1,7 +1,16 @@
 class Closure {
 
+    constructor() {
+
+    }
+
     test() {
-        let msg:string = 'Hello World';
+
+        let shows: string[] = ['attack on titan', 'gotham', 'arrow', 
+        'thirteen reasons why', 'stranger things','game of thrones', 
+        'riverdale', 'true blood', 'family guy', 'simpsons'];
+
+        let msg:string = shows[0];
         console.log(msg);
     }
 
